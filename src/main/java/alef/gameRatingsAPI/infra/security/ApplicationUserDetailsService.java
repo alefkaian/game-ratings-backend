ab3135c0aeab2a1a36b166eb898ffb076e7e5be0
alef <alef.kaian@gmail.com>
2025-09-23 16:04:32 -0300

    Refactor security

---
+++ src/main/java/alef/gameRatingsAPI/infra/security/ApplicationUserDetailsService.java
@@ -0,0 +1,21 @@
+package alef.gameRatingsAPI.infra.security;
+
+import alef.gameRatingsAPI.domain.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public ApplicationUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
